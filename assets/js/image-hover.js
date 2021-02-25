32749a445c1991f2a34d1348499b2a6b5f3ed125
Calvin <jinseinai@gmail.com>
2021-02-24 20:52:33 -0800

    fixing more

--- assets/js/image-hover.js
+++ assets/js/image-hover.js
@@ -1,14 +1,14 @@
 $('.card-figure').hover(
-  function () {
-    const image = $(this).find('.card-image');
-    const newImg = image.attr('data-hoverimage');
-    const currentImg = image.attr('src');
-    if (newImg && newImg !== '') image.attr('src', newImg);
-  },
-  function () {
-    const image = $(this).find('.card-image');
-    const newImg = image.attr('data-src');
-    const currentImg = image.attr('src');
-    if (newImg && newImg !== '') image.attr('src', newImg);
-  }
+    function () {
+        const image = $(this).find('.card-image');
+        const newImg = image.attr('data-hoverimage');
+        const currentImg = image.attr('src');
+        if (newImg && newImg !== '') image.attr('src', newImg);
+    },
+    function () {
+        const image = $(this).find('.card-image');
+        const newImg = image.attr('data-src');
+        const currentImg = image.attr('src');
+        if (newImg && newImg !== '') image.attr('src', newImg);
+    },
 );
